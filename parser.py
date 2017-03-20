b8145101cf35e4a2683d0e28a6915ce4f62e6b6e
Pat Mat <patryk.matyjasek@gmail.com>
2017-03-20 22:47:04 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -10,8 +10,8 @@ for repo in repos:
     repo_data = requests.get('https://api.github.com/repos/'+sys.argv[1]+'/'+repo['name']+'/git/trees/master?recursive=1')
     files = fl = json.loads(repo_data.text)
     for file in files:
-        if '.md' in f['path']:
-            md_file = requests.get('https://api.github.com/repos/'+sys.argv[1]+'/'+repo['name']+'/contents/'+f['path'])
+        if '.md' in file['path']:
+            md_file = requests.get('https://api.github.com/repos/'+sys.argv[1]+'/'+repo['name']+'/contents/'+file['path'])
             contents = md_file.text.split('\n')
             for line in contents:
                 if '|' in line and '/intel' in line:
