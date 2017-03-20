4ec6d7a1078d8f3151613bc0b9c50ad257ee2a29
Pat Mat <patryk.matyjasek@gmail.com>
2017-03-20 23:16:39 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,20 +2,20 @@ import requests
 import json
 import sys
 
-org_url = 'https://api.github.com/orgs/'+sys.argv[1]+'/repos'
+org_url = 'https://api.github.com/orgs/' + sys.argv[1] + '/repos'
 r = requests.get(org_url)
 repos = json.loads(r.text)
-
 for repo in repos:
     repo_data = requests.get(
-        'https://api.github.com/repos/' + sys.argv[1] + '/'+repo['name'] + '/git/trees/master?recursive=1')
+        'https://api.github.com/repos/' + sys.argv[1] + '/' + repo['name'] + '/git/trees/master?recursive=1')
     files = json.loads(repo_data.text)
-    for file in files:
-        if '.md' in file['path']:
+    for file in files['tree']:
+        if '.md' in file['path'] and not '.git' in file['path']:
             md_file = requests.get(
                 'https://api.github.com/repos/' + sys.argv[1] + '/' + repo['name'] + '/contents/' + file['path'])
             cont = requests.get(json.loads(md_file.text)['download_url'])
             contents = cont.text.split('\n')
+            print(repo['name'])
             for line in contents:
-                if '|' in line and '/intel' in line:
+                if '|' in line and '/intel/' in line:
                     print(line)
