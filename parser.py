dd7aa136c6bd7801c82307b192b4fec232688c04
Pat Mat <patryk.matyjasek@gmail.com>
2017-03-21 08:37:09 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,24 @@
-import requests
+import requests.auth
 import json
 import sys
 
-org_url = 'https://api.github.com/orgs/' + sys.argv[1] + '/repos'
-r = requests.get(org_url)
+org_url = 'https://api.github.com/orgs/' + sys.argv[1] + '/repos?page=1&per_page=100'
+user = sys.argv[2]
+pw = sys.argv[3]
+r = requests.get(org_url, auth=(user, pw))
 repos = json.loads(r.text)
 for repo in repos:
-    repo_data = requests.get(
-        'https://api.github.com/repos/' + sys.argv[1] + '/' + repo['name'] + '/git/trees/master?recursive=1')
-    files = json.loads(repo_data.text)
-    for file in files['tree']:
-        if '.md' in file['path'] and not '.git' in file['path']:
-            md_file = requests.get(
-                'https://api.github.com/repos/' + sys.argv[1] + '/' + repo['name'] + '/contents/' + file['path'])
-            cont = requests.get(json.loads(md_file.text)['download_url'])
-            contents = cont.text.split('\n')
-            print(repo['name'])
-            for line in contents:
-                if '|' in line and '/intel/' in line:
-                    print(line)
+    if 'collector' in repo['name']:
+        repo_data = requests.get(
+            'https://api.github.com/repos/' + sys.argv[1] + '/' + repo['name'] + '/git/trees/master?recursive=1', auth=(user, pw))
+        files = json.loads(repo_data.text)
+        for fil in files['tree']:
+            if '.md' in fil['path'] and not '.git' in fil['path']:
+                md_file = requests.get(
+                    'https://api.github.com/repos/' + sys.argv[1] + '/' + repo['name'] + '/contents/' + fil['path'], auth=(user, pw))
+                cont = requests.get(json.loads(md_file.text)['download_url'])
+                contents = cont.text.split('\n')
+                print(repo['name'])
+                for line in contents:
+                    if '|' in line and '/intel/' in line:
+                        print(line)
